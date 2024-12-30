c4f07338b2fdcbe9dc40787d859ef3065410e7a9
atakankizilyuce <atakankizilyuce@outlook.com>
2024-12-30 19:09:04 +0300

    Added valid anagram solution

---
+++ Array & Hashing/ValidAnagram/ValidAnagram/ValidAnagram/Program.cs
@@ -0,0 +1,30 @@
+﻿namespace ValidAnagram
+{
+    internal class Program
+    {
+        public static bool IsAnagram(string s, string t)
+        {
+            if (s.Length != t.Length)
+            {
+                return false;
+            }
+
+            char[] sArray = s.ToCharArray();
+            char[] tArray = t.ToCharArray();
+
+            Array.Sort(sArray);
+            Array.Sort(tArray);
+
+            var sString = new string(sArray);
+            var tString = new string(tArray);
+
+            return sString == tString;
+        }
+
+        static void Main(string[] args)
+        {
+            var result = IsAnagram("cat", "tac");
+            Console.WriteLine(result);
+        }
+    }
+}
