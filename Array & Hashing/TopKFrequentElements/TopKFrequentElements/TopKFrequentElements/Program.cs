e3992294522ae8415bbed82ea4983addef6c0943
atakankizilyuce <atakankizilyuce@outlook.com>
2024-12-31 21:35:24 +0300

    Added Top K Frequent Elements solution

---
+++ Array & Hashing/TopKFrequentElements/TopKFrequentElements/TopKFrequentElements/Program.cs
@@ -0,0 +1,38 @@
+﻿namespace TopKFrequentElements
+{
+    internal class Program
+    {
+        public static int[] TopKFrequent(int[] nums, int k)
+        {
+            var freqMap = new Dictionary<int, int>();
+            foreach (int num in nums)
+            {
+                if (!freqMap.ContainsKey(num))
+                {
+                    freqMap.Add(num, 0);
+                }
+                freqMap[num]++;
+            }
+
+            var frequencyList = new List<KeyValuePair<int, int>>(freqMap);
+            frequencyList.Sort((a, b) => b.Value.CompareTo(a.Value));
+
+            var result = new int[k];
+            for (var i = 0; i < k; i++)
+            {
+                result[i] = frequencyList[i].Key;
+            }
+
+            return result;
+        }
+
+        static void Main(string[] args)
+        {
+            var firstCase = new int[] { 1, 1, 1, 2, 2, 3 };
+            var firstCaseK = 2;
+
+            var result = TopKFrequent(firstCase, firstCaseK);
+            Console.WriteLine(result);
+        }
+    }
+}
