f63cbea823dcb74c653c1853fbbb06a7cc309e19
atakankizilyuce <atakankizilyuce@outlook.com>
2025-01-01 20:23:58 +0300

    Added Two Sum II - Input Array Is Sorted solution

---
+++ TwoPointers/TwoSumIIInputArrayIsSorted/TwoSumIIInputArrayIsSorted/TwoSumIIInputArrayIsSorted/Program.cs
@@ -0,0 +1,36 @@
+﻿namespace TwoSumIIInputArrayIsSorted
+{
+    internal class Program
+    {
+        public static int[] TwoSum(int[] numbers, int target)
+        {
+            var left = 0;
+            var right = numbers.Length - 1;
+            while (left < right)
+            {
+                var curSum = numbers[left] + numbers[right];
+                if (curSum > target)
+                {
+                    right--;
+                }
+                else if (curSum < target)
+                {
+                    left++;
+                }
+                else
+                {
+                    return new int[] { left + 1, right + 1 };
+                }
+            }
+            return new int[0];
+        }
+
+        static void Main(string[] args)
+        {
+            var firstCase = new int[] { 2, 7, 11, 15 };
+            var firstCaseTarget = 9;
+            var result = TwoSum(firstCase,firstCaseTarget);
+            Console.WriteLine(result);
+        }
+    }
+}
