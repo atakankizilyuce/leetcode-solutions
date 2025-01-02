a790406a3577edcd0fbee02cfc9fb52365bc180b
atakankizilyuce <atakankizilyuce@outlook.com>
2025-01-02 22:40:42 +0300

    Added Kids With the Greatest Number of Candies solution

---
+++ LeetCode75/KidsWithTheGreatestNumberOfCandies/KidsWithTheGreatestNumberOfCandies/Program.cs
@@ -0,0 +1,31 @@
+﻿namespace KidsWithTheGreatestNumberOfCandies
+{
+    internal class Program
+    {
+        public IList<bool> KidsWithCandies(int[] candies, int extraCandies)
+        {
+            var result = new List<bool>();
+            if (candies.Length == 0 || extraCandies == 0)
+            {
+                return result;
+            }
+
+            for (var i = 0; i < candies.Length; i++)
+            {
+                var candiesWithExtra = candies[i] + extraCandies;
+                var status = true;
+                for (var j = 0; j < candies.Length; j++)
+                {
+                    if (candiesWithExtra < candies[j])
+                    {
+                        status = false;
+                        break;
+                    }
+                }
+                result.Add(status);
+            }
+
+            return result;
+        }
+    }
+}
