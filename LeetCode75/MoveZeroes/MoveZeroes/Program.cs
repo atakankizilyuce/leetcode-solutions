b4ffbc2e458d73c2557aa50840b6988a931da838
atakankizilyuce <atakankizilyuce@outlook.com>
2025-01-05 00:23:06 +0300

    Added MoveZeroes solution

---
+++ LeetCode75/MoveZeroes/MoveZeroes/Program.cs
@@ -0,0 +1,46 @@
+﻿namespace MoveZeroes
+{
+    internal class Program
+    {
+        public static void MoveZeroes(int[] nums)
+        {
+            var isNonZeroExist = false;
+            for (var i = 0; i < nums.Length; i++)
+            {
+                if (nums[i] > 0)
+                {
+                    isNonZeroExist = true;
+                    break;
+                }
+            }
+
+            if (!isNonZeroExist)
+            {
+                return;
+            }
+
+            var zeroSize = 0;
+            for (var i = 0; i < nums.Length; i++)
+            {
+                if (nums[i] == 0)
+                {
+                    zeroSize++;
+                }
+
+                if (zeroSize > 0 && nums[i] != 0)
+                {
+                    var temp = nums[i];
+                    nums[i] = 0;
+                    nums[i - zeroSize] = temp;
+                }
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            var firstCase = new int[] { 0, 1, 0, 3, 12 };
+            MoveZeroes(firstCase);
+            Console.WriteLine(firstCase);
+        }
+    }
+}
