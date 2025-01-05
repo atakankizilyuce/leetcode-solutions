478c6642bcf15b1195261a4231679d994c0e1dc2
atakankizilyuce <atakankizilyuce@outlook.com>
2025-01-05 17:39:07 +0300

    Added Maximum Average Subarray I solution

---
+++ LeetCode75/MaximumAverageSubarrayI/MaximumAverageSubarrayI/MaximumAverageSubarrayI/Program.cs
@@ -0,0 +1,36 @@
+﻿namespace MaximumAverageSubarrayI
+{
+    internal class Program
+    {
+
+       // n == nums.length
+       // 1 <= k <= n <= 105
+       //-104 <= nums[i] <= 104
+
+        public static double FindMaxAverage(int[] nums, int k)
+        {
+            var sum = 0d;
+            for (var i = 0; i < k; i++)
+            {
+                sum += nums[i];
+            }
+
+            var maxSum = sum;
+            for (var i = k; i < nums.Length; i++)
+            {
+                sum += nums[i] - nums[i - k];
+                maxSum = maxSum >= sum ? maxSum : sum;
+            }
+
+            return maxSum / k;
+        }
+
+        static void Main(string[] args)
+        {
+            var firstCase = new int[] { 1, 12, -5, -6, 50, 3 };
+            var firstCaseK = 4;
+            var result = FindMaxAverage(firstCase, firstCaseK);
+            Console.WriteLine(result);
+        }
+    }
+}
