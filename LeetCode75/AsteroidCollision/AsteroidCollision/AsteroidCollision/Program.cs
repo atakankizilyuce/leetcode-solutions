a88e9202664eade9d519cb0ffbc12a1f569cd9fd
atakankizilyuce <atakankizilyuce@outlook.com>
2025-01-16 23:28:02 +0300

    Added Asteroid Collision solution

---
+++ LeetCode75/AsteroidCollision/AsteroidCollision/AsteroidCollision/Program.cs
@@ -0,0 +1,48 @@
+﻿namespace AsteroidCollision
+{
+    internal class Program
+    {
+        public static int[] AsteroidCollision(int[] asteroids)
+        {
+            var result = new List<int>();
+
+            foreach (var asteroid in asteroids)
+            {
+                var destroyed = false;
+                while (result.Count > 0 && asteroid < 0 && result[^1] > 0)
+                {
+                    var top = result[^1];
+                    if (top < -asteroid)
+                    {
+                        result.RemoveAt(result.Count - 1);
+                    }
+                    else if (top == -asteroid)
+                    {
+                        result.RemoveAt(result.Count - 1);
+                        destroyed = true;
+                        break;
+                    }
+                    else
+                    {
+                        destroyed = true;
+                        break;
+                    }
+                }
+
+                if (!destroyed)
+                {
+                    result.Add(asteroid);
+                }
+            }
+
+            return result.ToArray();
+        }
+
+        static void Main(string[] args)
+        {
+            var firstCase = new int[] { 5, 10, -5 };
+            var result = AsteroidCollision(firstCase);
+            Console.WriteLine(result);
+        }
+    }
+}
