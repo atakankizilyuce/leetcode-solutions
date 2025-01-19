bd50ae9a3225b41351d3269267ef290a2996ae00
atakankizilyuce <atakankizilyuce@outlook.com>
2025-01-19 14:59:27 +0300

    Added 1137. N-th Tribonacci Number solution

---
+++ LeetCode75/N-thTribonacciNumber/N-thTribonacciNumber/N-thTribonacciNumber/Program.cs
@@ -0,0 +1,32 @@
+﻿namespace N_thTribonacciNumber
+{
+    internal class Program
+    {
+        public static int Tribonacci(int n)
+        {
+            var result = 0;
+            if (n == 0)
+            {
+                return result;
+            }
+
+            int a = 0, b = 1, c = 1, d;
+            while (n-- > 2)
+            {
+                d = a + b + c;
+                a = b;
+                b = c;
+                c = d;
+            }
+            result = c;
+            return result;
+        }
+
+        static void Main(string[] args)
+        {
+            var firstCase = 25;
+            var result = Tribonacci(firstCase);
+            Console.WriteLine(result);
+        }
+    }
+}
