8e8fd0f074395fad1691d51896a6c5f47a8b40f8
atakankizilyuce <atakankizilyuce@outlook.com>
2025-02-07 23:55:17 +0300

    Added GetConcatenation solution

---
+++ Array & Hashing/GetConcatenation/GetConcatenation/GetConcatenation/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace GetConcatenation
+{
+    internal class Program
+    {
+        public static int[] GetConcatenation(int[] nums)
+        {
+            var ans = new int[nums.Length * 2];
+            for (var i = 0; i < nums.Length; i++)
+            {
+                ans[i] = nums[i];
+                ans[i + nums.Length] = nums[i];
+            }
+
+            return ans;
+        }
+
+        static void Main(string[] args)
+        {
+            var firstCase = new int[] { 1, 2, 1 };
+            var result = GetConcatenation(firstCase);
+        }
+    }
+}
