e73fd618f16118ba7a6065b16f0bb12d2d6065d3
atakankizilyuce <atakankizilyuce@outlook.com>
2025-02-09 19:30:48 +0300

    Added python and csharp folders

---
+++ Python/reverse_linked_list.py
@@ -0,0 +1,28 @@
+from typing import Optional
+# Definition for singly-linked list.
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+class Solution:
+    def reverseList(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        prev:ListNode = None
+        current:ListNode = head
+
+        while current:
+            next_node = current.next
+            current.next = prev
+            prev = current
+            current = next_node
+
+        return prev
+
+head = ListNode(1)
+head.next = ListNode(2)
+head.next.next = ListNode(3)
+head.next.next.next = ListNode(4)
+head.next.next.next.next = ListNode(5)
+
+solution = Solution()
+result = solution.reverseList(head)
