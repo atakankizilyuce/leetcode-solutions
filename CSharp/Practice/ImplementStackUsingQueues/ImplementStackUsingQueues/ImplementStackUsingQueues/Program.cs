09784caab1758ff454b42533edda16c7d51aabdb
atakankizilyuce <atakankizilyuce@outlook.com>
2025-02-11 22:25:10 +0300

    Added 225. Implement Stack using Queues solution

---
+++ CSharp/Practice/ImplementStackUsingQueues/ImplementStackUsingQueues/ImplementStackUsingQueues/Program.cs
@@ -0,0 +1,50 @@
+﻿namespace ImplementStackUsingQueues
+{
+    internal class Program
+    {
+        public class MyStack
+        {
+            private readonly Queue<int> q;
+
+            public MyStack()
+            {
+                q = new Queue<int>();
+            }
+
+            public void Push(int x)
+            {
+                q.Enqueue(x);
+                for (var i = q.Count - 1; i > 0; i--) 
+                {
+                    q.Enqueue(q.Dequeue());
+                }
+            }
+
+            public int Pop()
+            {
+                return q.Dequeue();
+            }
+
+            public int Top()
+            {
+                return q.Peek();
+            }
+
+            public bool Empty()
+            {
+                return q.Count == 0;
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            var stack = new MyStack();
+            stack.Push(1);
+            stack.Push(2);
+            stack.Push(3);
+            stack.Pop();
+            stack.Top();
+            stack.Empty();
+        }
+    }
+}
